fc48636c6c8f14ccff15941b7483c88f84f06a62
emsleyd <emsleyd@gmail.com>
2022-10-24 16:07:24 +0100

    creating new pokedex

--- src/pokegotchi/Pokegotchi.js
+++ src/pokegotchi/Pokegotchi.js
@@ -1,67 +1,26 @@
-import React, { useState, useEffect } from 'react'
-
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './pokeIndex.css'
-
-export default function Pokegotchi(props) {
-
-    React.useEffect(() => {
-        window.addEventListener("DOMContentLoaded", () => {
-          let generateBtn = document.querySelector('#generate-pokemon');
-          generateBtn.addEventListener('click', renderEverything);
-        });
-      });
-      function renderEverything() {
-        let allPokemonContainer = document.querySelector('#poke-container');
-        allPokemonContainer.innerText = "";
-        fetchKantoPokemon();
-      }
-      function fetchKantoPokemon() {
-        fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then(response => response.json()).then(function (allpokemon) {
-          allpokemon.results.forEach(function (pokemon) {
-            fetchPokemonData(pokemon);
-          });
-        });
-      }
-      function fetchPokemonData(pokemon) {
-        let url = pokemon.url; // <--- this is saving the pokemon url to a variable to use in the fetch. Example: https://pokeapi.co/api/v2pokemon/1/"
-        fetch(url).then(response => response.json()).then(function (pokeData) {
-          renderPokemon(pokeData);
-        });
-      }
-      function renderPokemon(pokeData) {
-        let allPokemonContainer = document.getElementById('poke-container');
-        let pokeContainer = document.createElement("div"); //div will be used to hold the data/details for indiviual pokemon.{}
-        pokeContainer.classList.add('ui', 'card');
-        createPokeImage(pokeData.id, pokeContainer);
-        let pokeNumber = document.createElement('p');
-        pokeNumber.innerText = `#${pokeData.id}`;
-        let pokeName = document.createElement('h4');
-        pokeName.innerText = pokeData.name;
-        pokeContainer.append(pokeName, pokeNumber); //appending all details to the pokeContainer div
-        allPokemonContainer.appendChild(pokeContainer); //appending that pokeContainer div to the main div which will hold all the pokemon cards
-      }
-      
-
-    return (
-
-      function createTypes(types, ul) {
-        types.forEach(function (type) {
-          let typeLi = document.createElement('li');
-          typeLi.innerText = type['type']['name'];
-          ul.append(typeLi);
-        });
-      },
-
-      function createPokeImage(pokeID, containerDiv) {
-        let pokeImgContainer = document.createElement('div');
-        pokeImgContainer.classList.add('sprite');
-        let pokeImage = document.createElement('img');
-        pokeImgContainer.classList.add('image');
-        pokeImage.srcset = `https://cdn.traction.one/pokedex/pokemon/${pokeID}.png`;
-        //   pokeImage.srcset = alt="Pokemon"
-      
-        pokeImgContainer.append(pokeImage);
-        containerDiv.append(pokeImgContainer);
-    }
-)}
+    
+    const Pokemon = ({ pokemon }) => (
+      <div className="card m-2 d-inline-flex text-center pokemon-card" style={{ width: '20rem' }}>
+        <Link to={`/${pokemon.id}`}>
+          <img src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`} className="card-img-top" alt="images" />
+          <p>
+            
+            {pokemon.id}
+          </p>
+          <h5 className="name">{pokemon.name}</h5>
+        </Link>
+      </div>
+    );
+    
+    Pokemon.propTypes = {
+      pokemon: PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+      }).isRequired,
+    };
+    
+    export default Pokemon;
