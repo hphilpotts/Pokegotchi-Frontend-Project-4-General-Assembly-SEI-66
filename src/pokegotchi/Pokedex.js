396a8a8656409ce3f12ac6d36f1462fe757c6813
emsleyd <emsleyd@gmail.com>
2022-10-25 17:59:43 +0100

    implementing api

--- src/pokegotchi/Pokedex.js
+++ src/pokegotchi/Pokedex.js
@@ -1,40 +1,43 @@
 import React from 'react'
-import { useEffect , useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
-import Pokegotchi from './Pokegotchi'; 
+import Pokegotchi from './Pokegotchi';
 import './pokedex.css'
 
 
 export default function Pokedex() {
 
-const [Pokegotchi, setPokegotchi] = useState([]);
-const [Error, setHasError] = useState([]);
+    const [PokegotchiList, setPokegotchiList] = useState([]);
+    const [Error, setHasError] = useState([]);
 
-// const [isEdit, setIsEdit] = useState(false);
-// const [currentPokegotchi, setCurrentPokegotchi] = useState({})
+    // const [isEdit, setIsEdit] = useState(false);
+    // const [currentPokegotchi, setCurrentPokegotchi] = useState({})
 
-useEffect(() => {
-    const fetchPokegotchi = async () => {
-      try {
-        const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151')
-        console.log(data)
-        setPokegotchi(data.results)
-      } catch (err) {
-        setHasError({ error: true, message: err.message })
-      }
-    }
-    fetchPokegotchi()
-  }, [])
+    useEffect(() => {
+        const fetchPokegotchiList = async () => {
+            try {
+                const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151')
+                // console.log(data)
+                setPokegotchiList(data.results)
+            } catch (err) {
+                setHasError({ error: true, message: err.message })
+            }
+        }
+        fetchPokegotchiList()
+    }, [])
 
-
-
-  return (
-    <div>
-        <h1>Pokegotchi-dex</h1>
+    return (
         <div>
-            pokegotchi
-        </div>
+            <h1>Pokegotchi-dex</h1>
+            <div>
+                {PokegotchiList.map(item => {
+                    return <Pokegotchi key={item.name} {...item} />
+                }
+            )}
+                
+            </div>
 
-    </div>
-  )
-  }
+        </div>
+    )
+}
+
--- src/pokegotchi/Pokegotchi.js
+++ src/pokegotchi/Pokegotchi.js
@@ -1,11 +1,54 @@
+import { ListItem } from '@mui/material'
 import React from 'react'
 import Pokedex from './Pokedex'
 import './pokedex.css'
 
-export default function Pokegotchi() {
-  return (
-    <div>Pokegotchi
-        <Pokedex />
-    </div>
-  )
+export default function Pokegotchi({url}) { // 
+
+  // const [Pokegotchi, setPokegotchi] = useState([]);
+  // const [Error, setHasError] = useState([]);
+
+  // make axios get request pass it url from line 6 of pokedex
+
+  useEffect(() => {
+    const fetchPokegotchi = async () => {
+        try {
+            const { data } = await axios.get({url})
+            console.log(data)
+            setPokegotchi(data.results)
+        } catch (err) {
+            setHasError({ error: true, message: err.message })
+        }
+    }
+    fetchPokegotchi()
+}, [])
+
+//  pokegotchiData
+
+
+  // console log to make sure response is .forms
+  // pass props keyword `${url}`
+  // or save key to variable and pass in
+  // destructuring
+  // let nextPath = {url}
+  // need state in axios to save data 
+  // set onto state
+  // map in return to get names/image of each.
+
+
+
+//   return (
+//     <div>
+//         <h1>Pokegotchi-dex</h1>
+//         <div>
+//             {PokegotchiList.map(item => {
+//                 return <Pokegotchi key={item.name} {...item} />
+//                 // ! NAME AND IMAGE
+//             }
+//         )}
+            
+//         </div>
+
+//     </div>
+// )
 }
