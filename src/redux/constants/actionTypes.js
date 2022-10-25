67946b67780462665f8efeec366a2bea04b2e113
emsleyd <emsleyd@gmail.com>
2022-10-25 09:11:14 +0100

    removing pokdedex files

--- src/redux/constants/actionTypes.js
+++
@@ -1,3 +0,0 @@
-export const GET_POKEMONS = 'GET_POKEMONS';
-export const GET_POKEMON = 'GET_POKEMON';
-export const CHANGE_FILTER = 'CHANGE_FILTER';
--- src/redux/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-import pokemon from './pokemon';
-import filter from './filter';
-
-export default combineReducers({ pokemon, filter });
