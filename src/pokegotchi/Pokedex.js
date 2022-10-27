3dec728662c94286264f80418bb0a380fb91f448
emsleyd <emsleyd@gmail.com>
2022-10-27 08:48:31 +0100

    to get up to date

--- src/pokegotchi/Pokedex.js
+++ src/pokegotchi/Pokedex.js
@@ -11,61 +11,71 @@ export default function Pokedex() {
     const [PokegotchiList, setPokegotchiList] = useState([]);
     const [Error, setHasError] = useState([]);
 
-    
+
 
     useEffect(() => {
         const PokegotchiList = async () => {
             try {
-                for(let i=1; i<=151; i++) {
-                const { data }  = await axios.get('https://pokeapi.co/api/v2/pokemon/' + i );
-                console.log(data.id)
-                // console.log(data.name)
-                // console.log(data.sprites.other["official-artwork"].front_default)
-                // console.log(data, "string")
+                for (let i = 1; i <= 151; i++) {
+                    const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/' + i);
+                    console.log(data.id)
+                    console.log(data.name)
+                    console.log(data.sprites.other["official-artwork"].front_default)
+                    // console.log(data, "string")
 
-                setPokegotchiList(data.id)
-                // console.log(setPokegotchiList)
-                
+                    setPokegotchiList(data.id)
+                    // console.log(setPokegotchiList)
 
-            }} catch (err) {
+
+                }
+            } catch (err) {
                 setHasError({ error: true, message: err.message })
             }
         }
         PokegotchiList()
     }, [])
 
-        
     return (
+         <div>
 
-        <div>
-            <h1>Some pokemon will appear here</h1>
-            <img src="img/pick1.png" className='logo-image' alt="pokemon-logo"/>
-            <div>
-                {PokegotchiList.map((pokemon) => {
-                    const {id, name, attributes } = pokemon
-                    return (
-                        <div>
-                            <h1>{id}</h1>
-                            <h1>{name}</h1>
-                            <ul>
-                                {attributes.map((attribute) => {
-                                    const {id, name, sprites = []} = attribute
-                                    const [sprite = null] = sprites
-                                    return (
-                                        <li key={id}>
-                                          <img src={sprite} alt={name}/> 
-                                          {name}        
-                                        </li>
-                                    )})
-                                }
-                            </ul>
-                            
-                        </div>
-                    )
-                })}
-            </div>
-
-        </div>
+         </div>
     )
-}
+// ! Spread syntax (...)?
+//! Object.values()?
+// ! object.keys to map through an object????
 
+{/* //         <div>
+//           {Object.keys(PokegotchiList).map((key, index) => {
+//             return (
+//               <div key={index}>
+//                 <h2>
+//                   {key}: {PokegotchiList[key]}
+//                 </h2>
+    
+//                 <hr />
+//               </div>
+//             );
+//           })}
+//             </div>
+    
+//           <br />
+//           <br />
+//           <br />
+    
+
+//         <div>
+
+//             {Object.values(PokegotchiList).map((value, id => {
+//                 return (
+//                     <div id={value}>
+//                         <h2>{id}</h2>
+//                         <hr />
+//           </div>
+//         );
+//       })}
+//     </div>
+</div>
+//   );
+// }
+ */}
+}
